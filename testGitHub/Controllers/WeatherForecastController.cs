16242edf44a3193b929a7965b913149d13a74dad
ahmad222jarad <ahmadjarad@gmai.com>
2025-11-08 00:19:35 +0300

    asd

--- testGitHub/Controllers/WeatherForecastController.cs
+++ testGitHub/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace testGitHub.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "€ﬁ›", "«·»»«·«»", "Chilly", "Ì·»·»Ì", "«‰ ‰« ", "Warm", "Balmy", "Hotss", "‘Ì”‘”Ì", "asdasd"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hotss", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;

9aeb3eb238b3a70df672ed13c2cea5b179037317
ahmad222jarad <ahmadjarad@gmai.com>
2025-11-09 14:29:05 +0300

    finalssws

--- testGitHub/Controllers/WeatherForecastController.cs
+++ testGitHub/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace TestGitHub.Controllers
         }
 
         // ===== GET v1.0 =====
-        [HttpGet("Zaid")]
+        [HttpGet("osama")]
         [MapToApiVersion("1.0")]
         public IEnumerable<WeatherForecast> GetV1()
         {
